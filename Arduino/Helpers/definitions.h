b8819a77a23394458cef7d6c911b298f2b25b350
sn0wyQ <soezforkm@gmail.com>
2021-07-25 13:14:20 +0300

    cpplint fixes 1

--- Arduino/Helpers/definitions.h
+++ Arduino/Helpers/definitions.h
@@ -38,7 +38,7 @@
 #define CMD_JUMP    1  // No arguments
 #define CMD_SHOOT   2  // No arguments
 #define CMD_AIM     3  // x, y -> how much to move mouse to in X and Y axis
-#define CMD_INFO    4  // n - number of NOT dormant enemies (hp, weapon, position)
+#define CMD_INFO    4  // n - number of NOT dormant enemies, then n player_infos
 #define CMD_UPDATE  5  // No arguments
 
 // Response codes
